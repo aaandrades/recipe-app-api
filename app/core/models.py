e84716b27ec8742e31b75171ff448f2fda34818d
Andres Andrade <aaandrades@outlook.com>
2022-04-04 20:23:44 -0500

    :sparkles: Adding super user function

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 
+# Behaviour for the model
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -18,6 +20,17 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+# Model for user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,7 +25,17 @@ class ModelTests(TestCase):
 
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
-        
+
         # Throw error if email is not provided
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'Testpass123')
+
+    def test_create_new_super_user(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@andresandrade.com',
+            'Testpass123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
