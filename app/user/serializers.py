0db5e13c8dfe0aa7e23653eca579c8fd85b6f035
Andres Andrade <aaandrades@outlook.com>
2022-04-24 23:15:35 -0500

    :sparkles: Added token auth to API

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,4 +1,6 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+# Extra languages supported by Django
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 
@@ -16,3 +18,34 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and authentica the user"""
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        # Access to the context of the request, which has all the
+        # information about the request
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+
+        # If user doesnt exist, throw a new error
+        if not user:
+            msg = _("Unable to authenticate with provided credentials")
+            raise serializers.ValidationError(msg, code="authentication")
+
+        # If user exists, return the user
+        attrs['user'] = user
+        return attrs
